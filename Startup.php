949f54aec5615dd61a647fecbcf2a18076d112c2
Jon Picchietti <jonpicchietti@gmail.com>
2012-05-28 18:57:51 -0500

    See extended description
    Server.class.php.old removed.
    Code to start the changed decode - not yet complete - have to fix
    something else first.
    Fixed php notice level errors for wsmpi.conf - still working on one more
    notice level error.

--- Startup.php
+++ Startup.php
@@ -7,6 +7,7 @@ function loadIni($file){
 			if($firstChar=="#" || $firstChar==""){array_splice($lines,$i-$removed,1);$removed++;} //if first character is a # then "ignore it".
 		}
 		for($i=0,$y=count($lines);$i<$y;$i++){
+			echo $lines[$i];
 			$pairs=explode('=',$lines[$i]); //take the key and the value
 			$startup[$pairs[0]]=$pairs[1]; //put the found key and value into an array element
 		}
@@ -18,9 +19,13 @@ $default=loadIni("wsmpi_default.conf");
 $startup=loadIni("wsmpi.conf");
 
 //variables we need to supply to the Classes.
-set_time_limit(($startup["time_limit"]) ? $startup["time_limit"] : $default['time_limit']);
+set_time_limit(($startup["time_limit"]) ? intval($startup["time_limit"]) : intval($default['time_limit']));
 $address=($startup["address"]) ? $startup["address"] : $default['address'];
-$port=($startup["port"]) ? $startup["port"] : $default['port'];
+$port=($startup["port"]) ? intval($startup["port"]) : intval($default['port']);
 $crossSite=($startup["crossSite"]) ? $startup["crossSite"] : $default['crossSite'];
 $verbose=($startup["verbose"]) ? $startup["verbose"] : $default['verbose'];
+
+//free memory
+$default=null;
+$startup=null;
 ?>
